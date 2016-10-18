bc55f6a8f1cd71596df2f84eee5326d81c139217
Peter Arato <it.arato@gmail.com>
2016-10-19 00:36:57 +0800

    Add rule generation.

--- ast_rule_builder.cc
+++ ast_rule_builder.cc
@@ -10,9 +10,12 @@
 
 using namespace std;
 
-struct ASTRuleGroup;
+Token ast_raw_token_to_token(string);
 
-enum ASTRuleTokenType { NOTHING, TOKEN, DEFINITION, GROUP, OPTION };
+struct ASTRuleConcatGroup;
+struct ASTRuleOrGroup;
+
+enum ASTRuleTokenType { NOTHING, TOKEN, DEFINITION, OR_GROUP, CONCAT_GROUP, OPTION, GROUP_CLOSE };
 
 ASTRuleTokenType token_type_of(string);
 
@@ -21,21 +24,27 @@ struct ASTRuleToken {
   union {
     Token token;
     string definition;
-    ASTRuleGroup* group;
+    ASTRuleOrGroup* or_group;
+    // Nothing is only defined in the struct's `type`.
   } value;
 };
 
-struct ASTRuleGroup {
-  vector<ASTRuleToken*> items;
+struct ASTRuleOrGroup {
+  vector<ASTRuleConcatGroup*> options;
   bool flagAnyNumberRepeat;
+};
+
+struct ASTRuleConcatGroup {
+  vector<ASTRuleToken*> tokens;
 };
 
 class AstRuleBuilder {
  private:
   ifstream fin;
-  map<string, ASTRuleGroup> rules;
+  map<string, ASTRuleOrGroup*> rules;
 
-  void parseRule(string&);
+  void parse_rule(string&);
+  ASTRuleOrGroup* parse_rule_group(istringstream&);
 
  public:
   AstRuleBuilder() : fin("./ast.rule"){};
@@ -47,30 +56,102 @@ void AstRuleBuilder::build() {
   string word;
   while (!fin.eof()) {
     getline(fin, word);
-    parseRule(word);
+    parse_rule(word);
+  }
+
+  for (auto it : rules) {
+    cout << it.first << " " << it.second->options.size() << endl;
   }
 }
 
-void AstRuleBuilder::parseRule(string& rule_raw) {
+void AstRuleBuilder::parse_rule(string& rule_raw) {
   istringstream is(rule_raw);
   string rule_name;
   is >> rule_name;
   rule_name.pop_back();
 
-  string raw_token;
+  ASTRuleOrGroup* group = parse_rule_group(is);
+  rules[rule_name] = group;
+}
+
+ASTRuleOrGroup* AstRuleBuilder::parse_rule_group(istringstream& is) {
+  ASTRuleOrGroup* group = new ASTRuleOrGroup{};
+  ASTRuleConcatGroup* currentConcatGroup = new ASTRuleConcatGroup{};
+
+  group->options.push_back(currentConcatGroup);
+
+  string raw;
   while (is.good()) {
-    is >> raw_token;
-    ASTRuleTokenType type = token_type_of(raw_token);
+    is >> raw;
+    ASTRuleTokenType type = token_type_of(raw);
+
+    if (type == ASTRuleTokenType::GROUP_CLOSE) {
+      if (raw.size() >= 2 && raw[1] == '*') {
+        group->flagAnyNumberRepeat = true;
+      }
+      break;
+    } else if (type == ASTRuleTokenType::OR_GROUP) {
+      ASTRuleOrGroup* _group = parse_rule_group(is);
+      ASTRuleToken* _token = new ASTRuleToken{};
+      _token->type = ASTRuleTokenType::OR_GROUP;
+      _token->value.or_group = _group;
+      currentConcatGroup->tokens.push_back(_token);
+    } else if (type == ASTRuleTokenType::NOTHING) {
+      ASTRuleToken* _token = new ASTRuleToken{};
+      _token->type = ASTRuleTokenType::NOTHING;
+      currentConcatGroup->tokens.push_back(_token);
+    } else if (type == ASTRuleTokenType::TOKEN) {
+      ASTRuleToken* _token = new ASTRuleToken{};
+      _token->type = ASTRuleTokenType::TOKEN;
+      _token->value.token = ast_raw_token_to_token(raw);
+      currentConcatGroup->tokens.push_back(_token);
+    } else if (type == ASTRuleTokenType::DEFINITION) {
+      ASTRuleToken* _token = new ASTRuleToken{};
+      _token->type = ASTRuleTokenType::DEFINITION;
+      _token->value.definition = raw;
+      currentConcatGroup->tokens.push_back(_token);
+    } else if (type == ASTRuleTokenType::OPTION) {
+      currentConcatGroup = new ASTRuleConcatGroup{};
+      group->options.push_back(currentConcatGroup);
+    } else {
+      throw "Unexpected AST rule type.";
+    }
   }
+
+  return group;
 }
 
 ASTRuleTokenType token_type_of(string raw) {
   if (raw[0] == 'T') return ASTRuleTokenType::TOKEN;
   if (raw[0] == '_') return ASTRuleTokenType::NOTHING;
-  if (raw[0] == '(') return ASTRuleTokenType::GROUP;
+  if (raw[0] == '(') return ASTRuleTokenType::OR_GROUP;
   if (raw[0] == '|') return ASTRuleTokenType::OPTION;
+  if (raw[0] == ')') return ASTRuleTokenType::GROUP_CLOSE;
 
   return ASTRuleTokenType::DEFINITION;
+}
+
+// void putback_word(istringstream& is, string word) {
+//   for (auto it = word.rbegin(); it != word.rend(); it++) {
+//     is.putback(*it);
+//   }
+// }
+
+Token ast_raw_token_to_token(string raw) {
+  raw = raw.substr(2, raw.size() - 3); // Remove trailing 'T(' and leading ')'.
+  auto eq_pos = raw.find('=');
+  string rawTokenType;
+  string value;
+
+  if (eq_pos != string::npos) {
+    rawTokenType = raw.substr(0, eq_pos);
+    value = raw.substr(eq_pos + 1);
+  } else {
+    rawTokenType = raw;
+    value = "";
+  }
+
+  return string_to_token(rawTokenType, value);
 }
 
 #endif
--- token.cc
+++ token.cc
@@ -33,6 +33,7 @@ class Token {
   TokenType type;
 
  public:
+  Token() {};
   Token(string _orig, TokenType _type) : orig(_orig), type(_type){};
   friend ostream &operator<<(ostream &, const Token &);
 };
@@ -40,6 +41,48 @@ class Token {
 ostream &operator<<(ostream &os, const Token &token) {
   os << "Token: " << token.orig;
   return os;
+}
+
+Token string_to_token(string type, string value) {
+  if (type == "PHP_START") {
+    return Token(value, TokenType::PHP_START);
+  } else if (type == "VARIABLE") {
+    return Token(value, TokenType::VARIABLE);
+  } else if (type == "KEYWORD") {
+    return Token(value, TokenType::KEYWORD);
+  } else if (type == "STRING") {
+    return Token(value, TokenType::STRING);
+  } else if (type == "PARENTHESIS_OPEN") {
+    return Token(value, TokenType::PARENTHESIS_OPEN);
+  } else if (type == "PARENTHESIS_CLOSE") {
+    return Token(value, TokenType::PARENTHESIS_CLOSE);
+  } else if (type == "BRACKET_OPEN") {
+    return Token(value, TokenType::BRACKET_OPEN);
+  } else if (type == "BRACKET_CLOSE") {
+    return Token(value, TokenType::BRACKET_CLOSE);
+  } else if (type == "SEMICOLON") {
+    return Token(value, TokenType::SEMICOLON);
+  } else if (type == "COMMA") {
+    return Token(value, TokenType::COMMA);
+  } else if (type == "OP_ASSIGNMENT") {
+    return Token(value, TokenType::OP_ASSIGNMENT);
+  } else if (type == "OP_PLUS") {
+    return Token(value, TokenType::OP_PLUS);
+  } else if (type == "OP_MINUS") {
+    return Token(value, TokenType::OP_MINUS);
+  } else if (type == "OP_MULTIPLY") {
+    return Token(value, TokenType::OP_MULTIPLY);
+  } else if (type == "OP_DIVIDE") {
+    return Token(value, TokenType::OP_DIVIDE);
+  } else if (type == "OP_MODULO") {
+    return Token(value, TokenType::OP_MODULO);
+  } else if (type == "SPEC_NAME") {
+    return Token(value, TokenType::SPEC_NAME);
+  } else if (type == "NUMERIC") {
+    return Token(value, TokenType::NUMERIC);
+  }
+
+  throw "Invalid type";
 }
 
 #endif
